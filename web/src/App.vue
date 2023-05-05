df1832f97bae31dd77c22d50938199a5a78b2644
szymci0 <szymonbetk@gmail.com>
2023-05-05 23:39:22 +0200

    Landing page styling tweaks

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    <div>
+  <div class="body">
+    <div class="border-bottom">
       <NavBar/>
     </div>
     <div class="container mt-3">
-      <router-view />
+      <transition name="slide-fade">
+        <router-view />
+      </transition>
     </div>
   </div>
 </template>
@@ -18,3 +20,22 @@ export default {
   },
 }
 </script>
+<style lang="scss">
+body {
+  background-color: #363636;
+  color: white;
+}
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
