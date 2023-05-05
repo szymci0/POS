cc221e62fcec7ed719016320ef39a614d4cc3c15
szymci0 <szymonbetk@gmail.com>
2023-05-05 23:14:07 +0200

    Styling tweaks for navbar

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -1,17 +1,12 @@
 <template>
     <div class="navbar d-flex justify-content-start align-items-center p-3 gap-2">
-        <button
-            v-for="link in navLinks" 
-            @click="navigate(link.path)"
-            :key="link.name"
-            class="nav-btn"
-        >
-            {{link.name.toUpperCase()}}
+        <button v-for="link in navLinks" @click="navigate(link.path)" :key="link.name" class="nav-btn">
+            {{ link.name.toUpperCase() }}
         </button>
     </div>
 </template>
 <script>
-import {navigation} from './nav';
+import { navigation } from './nav';
 export default {
     name: 'NavBar',
     data() {
@@ -21,27 +16,35 @@ export default {
     },
     methods: {
         navigate(path) {
-            this.$router.push({path: path})
+            this.$router.push({ path: path })
         }
     }
 }
 </script>
 <style lang="scss">
 .navbar {
-  background-color: white;
-  border-radius: 1px 0 0 0;
-  border-color: grey;
-  border-style: solid;
-  overflow: hidden;
-  width: 101%;
+    background-color: #F4D03F;
+    background-image: linear-gradient(-132deg, #F4D03F 10%, #16A085 100%);
+    background-size: 100%;
+    opacity: 80%;
+    overflow: hidden;
+    width: 101%;
+    transition: all 1s linear;
+    &:hover {
+        transition: all 1s linear;
+        background-size: 300%;
+    }
 }
+
 .nav-btn {
     border: 0;
-    background-color: white;
+    background-color: transparent;
     font-weight: bold;
+    color: white;
 
     &:hover {
-        opacity: 50%;
+        transition: all 0.2s linear;
+        color: #F4D03F;
     }
 }
 </style>
