c98496051053f2e33662ab22d6e07fa19f2ab176
szymci0 <szymonbetk@gmail.com>
2023-05-05 18:47:01 +0200

    Init vue router

--- api/app.py
+++ api/app.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from endpoints import user
-
+from api.config import init_database()
 app = FastAPI()
 
 app.include_router(user.router)
+
+@app.on_event("startup")
+async def start_database():
+    await init_database()
 
 
 @app.get("/")
--- api/config.py
+++ api/config.py
@@ -1,20 +1,5 @@
-from pydantic import BaseSettings
+from typing import Optional
 
 
-class Settings(BaseSettings):
-    DATABASE_URL: str
-    MONGO_INITDB_DATABASE: str
-
-    JWT_PUBLIC_KEY: str
-    JWT_PRIVATE_KEY: str
-    REFRESH_TOKEN_EXPIRES_DB: int
-    ACCESS_TOKEN_EXPIRES_IN: int
-    JWT_ALGORITH: str
-
-    CLIENT_ORIGIN: str
-
-    class Config:
-        env_file = "./.env"
-
-
-settings = Settings()
+def init_database():
+    return
