7a107a825d079942121f7bc8b192e0e4e3b66b48
szymci0 <szymonbetk@gmail.com>
2023-05-05 20:03:00 +0200

    Fix main page

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,16 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
