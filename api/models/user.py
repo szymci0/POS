a7c5594bacf3d63109dfcafaef4a6f6a8832c3c1
szymci0 <szymonbetk@gmail.com>
2023-05-08 23:40:57 +0200

    Configure User model with JWT auth

--- api/models/user.py
+++ api/models/user.py
@@ -1,33 +1,57 @@
-from pydantic import BaseModel
-from api import app
-from passlib.context import CryptContext
+from fastapi import Response
+from bson import ObjectId
+from fastapi_users import models
+from fastapi_users.models import BaseUserDB
+from mongoengine import DynamicDocument, UUIDField
+from fastapi_users.authentication import JWTAuthentication
+from pydantic import validator
+from typing import Optional, Any
+from abc import ABC
+from api.app import app
+from datetime import datetime
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+class User(models.BaseUser):
+    firstname: Optional[str] = None
+    lastname: Optional[str] = None
+    created_at: Optional[datetime] = None
+    last_seen: Optional[datetime] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+    
+    @validator("created_at", pre=True, always=True)
+    def default_created_at(cls, v):
+        return v or datetime.now()
+    
+
+class UserDB(models.BaseOAuthAccountMixin, User, models.BaseUserDB):
+    pass
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+class UserUpdate(User, models.BaseUserUpdate):
+    pass
 
 
-class TokenData(BaseModel):
-    username: str | None = None
+class Users(DynamicDocument):
+    id = UUIDField()
+    meta = {
+        'id_field': 'id',
+    }
 
 
-class User(BaseModel):
-    username: str
-    email: str
-    disabled: bool
+class CustomJWTAuthentication(JWTAuthentication, ABC):
+    async def get_login_response(
+            self, user: BaseUserDB, response: Response,
+    ) -> Any:
+        user.last_seen = datetime.now()
+        await app.users.db.update(user)
+        login_response = await super().get_login_response(user, response)
+        return login_response
+    
 
-
-class UserInDB(User):
-    hashed_password: str
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
+DAY = 60 * 60 * 24
+jwt_authentication = CustomJWTAuthentication(
+    secret='c8ece35c-8c3b-4ce5-9f32-46ac5a83a234', lifetime_seconds=DAY, tokenUrl="/auth/jwt/login"
+)
